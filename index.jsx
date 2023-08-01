3b0da46104fb685b83df23f9cecebc193308822e
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2023-08-01 17:22:34 -0400

    add Vans component

--- index.jsx
+++ index.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 import Home from './pages/Home'
 import About from './pages/About'
+import Vans from './pages/Vans'
+import "./server"
 
 function App() {
   return (
@@ -12,11 +14,13 @@ function App() {
         <Link to="/">#VANLIFE</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ pages/Vans.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+
+export default function Vans() {
+    const [vans, setVans] = useState([])
+
+    useEffect(() => {
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    },[])
+
+    const vanElements = vans.map(van => (
+        <div key={van.id} className="van-tile">
+            <img src={van.imageUrl} />
+            <div className="van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}<span>/day</span></p>
+            </div>
+            <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </div>
+    ))
+
+    return (
+        <div className="van-list-container">
+            <div className="van-list">
+                {vanElements}
+            </div>
+        </div>
+    )
+}
