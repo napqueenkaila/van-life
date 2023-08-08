66d5c1835c3f9a706530721d6d282c0c67fc610c
napqueenkaila <125321591+napqueenkaila@users.noreply.github.com>
2023-08-08 17:30:50 -0400

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from "./pages/Vans/Vans"
+import Vans from "./pages/vans/Vans"
 import VanDetail from "./pages/Vans/VanDetail"
 import Login from "./pages/Login"
 import Dashboard from "./pages/Host/Dashboard"
